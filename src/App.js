14f53b98f69def0500e5d6ebc6252d856c2faa8e
lukenetti3 <luke.netti3@gmail.com>
2020-11-27 19:02:14 -0500

    last update

--- src/App.js
+++ src/App.js
@@ -21,13 +21,14 @@ const ADD_CARD = gql`
 
 export default function App() {
   const [values, setValues] = useState({question: "", answer: ""})
-  const [cardList, setCardList] = useState([])
+  const [cards, setCards] = useState([])
+  const [isCards, setIsCards] = useState(false)
   const [insert_cards_one, {mutatedData}] = useMutation(ADD_CARD)
-
   const { loading, error, data } = useQuery(CARDS)
   
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
+  
 
   function handleChange(event) {
     const {name, value} = event.target
@@ -36,32 +37,35 @@ export default function App() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    setCardList(prevCard => [...prevCard, values])
     insert_cards_one({variables: { q: values.question, a: values.answer}})
+    setCards(prevCard => [...prevCard, values])
     setValues({question: "", answer: ""})
+    window.location.reload(false);
   }
 
-  const qas = data.cards.map(card => (
-    <div>
+
+  function showCards() {
+    setIsCards(!isCards)
+  } 
+
+  const viewCards =  data.cards.map(card => (
+    <div key={card.question}>
       <p>Question: {card.question}</p>
       <p>Answer: {card.answer}</p>
     </div>
-  ))
-
-  // useEffect(() => {
-  //   setCardList(data.cards)  
-  // },[])
+    ))
 
   return (
     <div>
       <h1>Welcome to Luke's flash cards</h1>
-      <h3>Enter a new flashcard</h3>
+      <h3>New flashcard</h3>
       <form onSubmit={handleSubmit}>
         <textarea placeholder="Question" name="question" onChange={handleChange} value={values.question}/>
         <textarea placeholder="Answer" name="answer" onChange={handleChange} value={values.answer}/>
         <button type="submit">Submit</button>
       </form>
-      { qas }
+      <button onClick={showCards}>View Cards</button>
+      {isCards ? viewCards : null}
     </div>
   );
 }
