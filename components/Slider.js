671e7976d53970e0b5ce3936cd572b400e0dc14f
lukenetti3 <luke.netti3@gmail.com>
2020-11-29 19:57:11 -0500

    Styled cards

--- components/Slider.js
+++ components/Slider.js
@@ -1,19 +1,21 @@
 import React from "react"
+import { BiRightArrowCircle, BiLeftArrowCircle} from "react-icons/bi"
 
 function Slider(props) {
   
   return(
     
-    <div className="mySlides">
-      <div className="singleCard">
+    <div className="slides-container">
+      <BiLeftArrowCircle style={{fontSize: "3em", color: "#3C91E6"}}/>
+      <div className="single-card">
+        <h4>Question:</h4>
         <p>{props.question}</p>
+        <h4>Answer:</h4>
         <p>{props.answer}</p>
         {console.log(props.length)}
-        <p style={{textAlign: "center"}}>{props.length}</p>
+        <p style={{textAlign: "center"}}>{props.currLength}/{props.total}</p>
       </div>
-      <div>
-        <p>{props.currLength}/{props.total}</p>
-      </div>
+      <BiRightArrowCircle style={{fontSize: "3em", color: "#3C91E6"}}/>
     </div>
 
   )
