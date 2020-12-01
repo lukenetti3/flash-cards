b9d1da95dd1f4ca5d968e2d3d373e02fab076f58
lukenetti3 <luke.netti3@gmail.com>
2020-11-30 20:45:02 -0500

    Needs work on the slider

--- components/Slider.js
+++ components/Slider.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { BiRightArrowCircle, BiLeftArrowCircle} from "react-icons/bi"
-import { Slide } from 'react-slideshow-image'
+import { Carousel } from 'react-responsive-carousel';
 
 function Slider(props) {
 
   const cards = props.arr
     
   return(
-    <div>
     <div>
       {cards.map((card, i) => (
       <div className="slides-container" key={i}>
@@ -22,25 +21,6 @@ function Slider(props) {
       </div>
       ))}
     </div>
-
-    <Slide easing="ease">
-          <div className="each-slide">
-            <div style={{backgroundColor: "blue"}}>
-              <span>Slide 1</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{backgroundColor: "red"}}>
-              <span>Slide 2</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{backgroundColor: "green"}}>
-              <span>Slide 3</span>
-            </div>
-          </div>
-        </Slide>
-        </div>
   )
 }
 
