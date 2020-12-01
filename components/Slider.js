a92275c08464aa7daa38d1536ef7bdc4d9bda06f
lukenetti3 <luke.netti3@gmail.com>
2020-12-01 08:38:32 -0500

    Working slider with react-elastic-carousel

--- components/Slider.js
+++ components/Slider.js
@@ -1,26 +1,24 @@
 import React from "react"
 import { BiRightArrowCircle, BiLeftArrowCircle} from "react-icons/bi"
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-elastic-carousel'
 
 function Slider(props) {
 
   const cards = props.arr
     
   return(
-    <div>
+    <Carousel>
       {cards.map((card, i) => (
       <div className="slides-container" key={i}>
-        <BiLeftArrowCircle style={{fontSize: "3em", color: "#3C91E6"}} onClick={() => nextSlide(-1)}/>
         <div className="single-card">
           <h4>Question:</h4>
           <p>{card.question}</p>
           <h4>Answer:</h4>
           <p>{card.answer}</p>
         </div>
-        <BiRightArrowCircle style={{fontSize: "3em", color: "#3C91E6"}} onClick={() => nextSlide(1)}/>
       </div>
       ))}
-    </div>
+    </Carousel>
   )
 }
 
