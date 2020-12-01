3a86a5b4571ef58df1ee3ad3e3af2d6f5a8c8abd
lukenetti3 <luke.netti3@gmail.com>
2020-11-30 20:35:25 -0500

    Attempted react-slideshow-image

--- components/Slider.js
+++ components/Slider.js
@@ -1,36 +1,46 @@
 import React from "react"
 import { BiRightArrowCircle, BiLeftArrowCircle} from "react-icons/bi"
-
+import { Slide } from 'react-slideshow-image'
 
 function Slider(props) {
 
   const cards = props.arr
-  const slideIndex = 1
-  dispSlides(slideIndex)
-
-  function prevSlide() {
-    dispSlides
-  }
-
-  function dispSlides() {
-    const singleCard = document.getElementsByClassName("single-card")
-  }
-
+    
   return(
+    <div>
     <div>
       {cards.map((card, i) => (
       <div className="slides-container" key={i}>
-        <BiLeftArrowCircle style={{fontSize: "3em", color: "#3C91E6"}} onClick={prevSlide}/>
+        <BiLeftArrowCircle style={{fontSize: "3em", color: "#3C91E6"}} onClick={() => nextSlide(-1)}/>
         <div className="single-card">
           <h4>Question:</h4>
           <p>{card.question}</p>
           <h4>Answer:</h4>
           <p>{card.answer}</p>
         </div>
-        <BiRightArrowCircle style={{fontSize: "3em", color: "#3C91E6"}}/>
+        <BiRightArrowCircle style={{fontSize: "3em", color: "#3C91E6"}} onClick={() => nextSlide(1)}/>
       </div>
       ))}
     </div>
+
+    <Slide easing="ease">
+          <div className="each-slide">
+            <div style={{backgroundColor: "blue"}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{backgroundColor: "red"}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{backgroundColor: "green"}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+        </div>
   )
 }
 
--- empty.js
+++ empty.js
@@ -1,5 +1,25 @@
-<div>
-  {cards.map((card, i) => (
-    isCards && <Slider key={card.id} question={card.question} answer={card.answer} currLength={i+1} total={cards.length}/>
-  ))}
-</div>
+
+
+  useEffect(() => {
+    var slideIndex = 1
+      dispSlides(slideIndex)
+
+      function nextSlide(n) {
+        dispSlides(slideIndex += n)
+      }
+
+      function dispSlides(n) {
+        const allCards = document.getElementsByClassName("single-card")
+        if(n > allCards.length) {
+          slideIndex = 1
+        }
+        if(n < 1) {
+          slideIndex = allCards.length
+        }
+
+        for (var i = 0; i < allCards.length; i++) {
+          allCards[i].style.display = "none"
+        }
+        console.log(allCards)
+      }
+  },[])
